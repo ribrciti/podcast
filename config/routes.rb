f4aa26964135fb5acc05efe8a1dbc448e5722669
ribrciti <rbciti@msn.com>
2015-09-22 07:50:32 -0700

    Added devise gem and podcast_user model and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   
+  devise_for :podcast_users
   root 'welcome#index'
 
   # Example of regular route:
---
+++ test/models/podcast_user_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class PodcastUserTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
